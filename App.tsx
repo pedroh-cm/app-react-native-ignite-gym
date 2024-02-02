394453ec6d709a90dd96f7deea96eba18d3ade91
Pedro Henrique Carlos Morinelli <pedro.morinelli@cobasi.com.br>
2024-02-01 23:02:13 -0300

    initial setup for app with native base components

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { View, Text, StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { SignIn } from '@screens/SignIn';
+
+import { Loading } from '@components/Loading';
+
+import { THEME } from './src/theme';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  return (
+    <NativeBaseProvider isSSR={false} theme={THEME}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+       {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
+
